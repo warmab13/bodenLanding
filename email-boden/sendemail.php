5407327e7b8e0921f34840a8ca08e52f81b18c06
Alonso Salcido <alonsosalcido13@gmail.com>
2016-12-27 16:19:30 -0700

    senderemailkey and index.php

--- email-boden/sendemail.php
+++ email-boden/sendemail.php
@@ -3,16 +3,16 @@ header("Access-Control-Allow-Origin: *");
 require_once 'src/Mandrill.php';
 
 try {
-    $mandrill = new Mandrill('_pdH4ZMORJjpfKLCL2lxpQ');
+    $mandrill = new Mandrill('gNY9JCZnJKFIAfE9jkjdmQ');
     $message = array(
-        'html' => '<!DOCTYPE html><html><head><meta charset="utf-8"></head><body><div style="padding-left:50px;padding-right:50px;"><body><h1>Has recibido un nuevo mensaje desde el contacto de la pagina web, estos son los datos del cliente:</h1><h2>Nombre: '.$_POST["nombre"].'</h2><h3>Correo Electronico: '.$_POST["email"].'</h3><h4>Telefono: '.$_POST["phone"].'<h5>Mensaje del cliente: '.$_POST["messages"].'</h5><p>Ciudad de donde cotiza: '.$_POST["city"].'.</p><p>$'.$_POST["inversion"].' cantidad de inversion del cliente.</p></body></html>',
+        'html' => '<!DOCTYPE html><html><head><meta charset="utf-8"></head><body><div style="padding-left:50px;padding-right:50px;"><body><h1>Has recibido un nuevo mensaje desde el contacto de la pagina web, estos son los datos del cliente:</h1><h2>Nombre: '.$_POST["nombre"].'</h2><h3>Correo Electronico: '.$_POST["email"].'<h5>Mensaje del cliente: '.$_POST["messages"]',
         'text' => '',
         'subject' => 'Contacto ',
-        'from_email' => "noreply@trinova.com.mx",
+        'from_email' => "noreply@bpr.com.mx",
         'from_name' => 'El equipo de Trinova',
         'to' => array(
             array(
-                'email' => "ventas@trinova.com.mx",
+                'email' => "ventas@bpr.com.mx",
                 // 'name' => 'Recipient Name',
                 'type' => 'to'
             )
